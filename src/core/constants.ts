8945c60339137d82425c8bc7a4309d8b7ac5a057
marcopeocchi <marco.piovanello@gmail.com>
2022-09-10 11:24:02 +0200

    code refactoring

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,6 +1,6 @@
 export const httpHeaders = {
     headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36 Edg/103.0.1264.62'
     },
 }
 
