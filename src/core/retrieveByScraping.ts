571ed8acf78e393747dc1970c7496c3a2845048b
marcopeocchi <marco.piovanello@gmail.com>
2022-09-16 16:57:31 +0200

    better scraping criterias

--- src/core/retrieveByScraping.ts
+++ src/core/retrieveByScraping.ts
@@ -3,12 +3,14 @@ import { googleDocsArtifacts, googleDocsArtifactsPath, httpHeaders } from './con
 import myHttp from './http';
 
 import LRUCache from 'lru-cache';
-import { APIResponse, Artifact, GenshinWeapons, Weapon } from '../types/global';
+import { APIResponse, Artifact, GenshinWeapons, SearchStrategy, Weapon } from '../types/global';
 import {
     artifactsMultipleSearchStrategy,
     weaponsMultipleSearchStrategy,
 } from './strategies';
-import { Decouple } from './utils';
+import { bumpClassBy, Decouple } from './utils';
+
+const CLASS_BUMPS = 50
 
 const cache = new LRUCache<string, Weapon[] | Artifact[]>({
     max: 50,
@@ -48,42 +50,52 @@ export async function findWeapons(type: GenshinWeapons): Promise<APIResponse<Wea
     const tbody = $('tbody')[typeMapping[type]]
     const trs = $(tbody).find('tr')
 
-    const weapons: Array<Weapon> = Array.from(trs).map((tr, idx) => {
-        const name = $(tr).find(Decouple($(weaponsMultipleSearchStrategy.name))).first().text()
-        // img sists on the next tr 🙄
-        const img = (idx + 1) < trs.length ? $(trs[idx + 1]).find('img').attr('src') : ''
+    const fetchClosure = (weaponsMultipleSearchStrategy: SearchStrategy<Weapon>) => {
+        const weapons: Array<Weapon> = Array.from(trs).map((tr, idx) => {
+            const name = $(tr).find(Decouple($(weaponsMultipleSearchStrategy.name))).first().text()
+            // img sists on the next tr 🙄
+            const img = (idx + 1) < trs.length ? $(trs[idx + 1]).find('img').attr('src') : ''
 
-        const [mainStats, subStats] = $(tr).find(Decouple($(weaponsMultipleSearchStrategy.mainStat)))
-        const passives = $(tr).find(Decouple($(weaponsMultipleSearchStrategy.passiveEffect))).first().text()
+            const [mainStats, subStats] = $(tr).find(Decouple($(weaponsMultipleSearchStrategy.mainStat)))
+            const passives = $(tr).find(Decouple($(weaponsMultipleSearchStrategy.passiveEffect))).first().text()
 
-        if (name && mainStats && subStats && passives) {
-            const [baseATKlv1, baseATKlv90] = $(mainStats).text().replace('Base ATK:', '').split('/')
-            const [subStatType, numValues] = $(subStats).text().split(':')
-            const [subStatLv1, subStatLv90] = numValues.split('/')
+            if (name && mainStats && subStats && passives) {
+                const [baseATKlv1, baseATKlv90] = $(mainStats).text().replace('Base ATK:', '').split('/')
+                const [subStatType, numValues] = $(subStats).text().split(':')
+                const [subStatLv1, subStatLv90] = numValues.split('/')
 
-            return {
-                name: name,
-                img: img,
-                mainStat: {
-                    baseATKlv1: Number(baseATKlv1.trim()),
-                    baseATKlv90: Number(baseATKlv90.trim())
-                },
-                subStat: {
-                    type: subStatType,
-                    valueLv1: Number(subStatLv1),
-                    valueLv90: Number(subStatLv90)
-                },
-                passiveEffect: passives
+                return {
+                    name: name,
+                    img: img,
+                    mainStat: {
+                        baseATKlv1: Number(baseATKlv1.trim()),
+                        baseATKlv90: Number(baseATKlv90.trim())
+                    },
+                    subStat: {
+                        type: subStatType,
+                        valueLv1: Number(subStatLv1),
+                        valueLv90: Number(subStatLv90)
+                    },
+                    passiveEffect: passives
+                }
             }
+            return undefined
+        }).filter(Boolean) as Weapon[]
+
+        return {
+            data: weapons
         }
-        return undefined
-    }).filter(Boolean) as Weapon[]
-
-    cache.set(type, weapons)
-
-    return {
-        data: weapons
     }
+
+    for (let i = 0; i <= CLASS_BUMPS; i++) {
+        const artifacts = fetchClosure(bumpClassBy<Weapon>(weaponsMultipleSearchStrategy, i))
+        if (artifacts) {
+            cache.set('artifacts', artifacts.data)
+            return artifacts
+        }
+    }
+
+    return { data: [] }
 }
 
 /**
@@ -107,28 +119,38 @@ export async function findArtifacts(): Promise<APIResponse<Artifact>> {
     const tbody = $('tbody')[7]
     const trs = $(tbody).find('tr')
 
-    const artifacts: Array<Artifact> = Array.from(trs).map((tr, idx) => {
-        const name = $(tr).find(Decouple($(artifactsMultipleSearchStrategy.name))).first().text()
-        // img sists on the next tr 🙄
-        const img = (idx + 1) < trs.length ? $(trs[idx + 1]).find('img').attr('src') : ''
+    const fetchClosure = (artifactsMultipleSearchStrategy: SearchStrategy<Artifact>) => {
+        const artifacts: Array<Artifact> = Array.from(trs).map((tr, idx) => {
+            const name = $(tr).find(Decouple($(artifactsMultipleSearchStrategy.name))).first().text()
+            // img sists on the next tr 🙄
+            const img = (idx + 1) < trs.length ? $(trs[idx + 1]).find('img').attr('src') : ''
 
-        const twoPieces = $(tr).find(Decouple($(artifactsMultipleSearchStrategy.twoPieces))).text()
-        const fourPieces = $(tr).find(Decouple($(artifactsMultipleSearchStrategy.fourPieces))).text()
+            const twoPieces = $(tr).find(Decouple($(artifactsMultipleSearchStrategy.twoPieces))).text()
+            const fourPieces = $(tr).find(Decouple($(artifactsMultipleSearchStrategy.fourPieces))).text()
 
-        if (name && twoPieces && fourPieces) {
-            return {
-                name: name,
-                img: img,
-                twoPieces: twoPieces,
-                fourPieces: fourPieces,
+            if (name && twoPieces && fourPieces) {
+                return {
+                    name: name,
+                    img: img,
+                    twoPieces: twoPieces,
+                    fourPieces: fourPieces,
+                }
             }
+            return undefined
+        }).filter(Boolean) as Artifact[]
+
+        return {
+            data: artifacts
         }
-        return undefined
-    }).filter(Boolean) as Artifact[]
-
-    cache.set('artifacts', artifacts)
-
-    return {
-        data: artifacts
     }
+
+    for (let i = 0; i <= CLASS_BUMPS; i++) {
+        const artifacts = fetchClosure(bumpClassBy<Artifact>(artifactsMultipleSearchStrategy, i))
+        if (artifacts) {
+            cache.set('artifacts', artifacts.data)
+            return artifacts
+        }
+    }
+
+    return { data: [] }
 }
--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,3 +1,36 @@
+import { SearchStrategy } from "../types/global";
+
 export function Decouple(T: any): string {
     return (T as unknown) as string
+}
+
+/**
+ * Since google sheets bumps classnames by 1 on every build/update
+ * deconstruct the object and bump each classname by one
+ * Classnames are defined as s[n] where n is a positive integer,
+ * e.g. s79 is a class name. This function bumps s[n] to s[n+1],
+ * e.g. s79 -> s80.
+ * Then, finally returns the object
+ * @param strategy generic search strategy object, either for Weapons or Artifacts
+ * @param bumpAddend how many times the class should be bumped
+ * @returns 
+ */
+export function bumpClassBy<T>(strategy: SearchStrategy<T>, bumpAddend: number): SearchStrategy<T> {
+    if (bumpAddend === 0) {
+        return strategy
+    }
+    const copy = { ...strategy }
+    for (const key in strategy) {
+        if (Object.prototype.hasOwnProperty.call(strategy, key)) {
+            if (key !== 'img') {
+                const bumped = strategy[key]
+                    .replace('td.s', '')
+                    .split(',')
+                    .map(val => String(Number(val) + bumpAddend))
+                    .reduce((curr, next) => `${curr}, td.s${next}`)
+                copy[key] = `td.s${bumped}`
+            }
+        }
+    }
+    return copy
 }
