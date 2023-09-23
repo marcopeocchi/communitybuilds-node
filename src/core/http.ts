8612d0ca9aff17d8a0801fa569f6e683016b705a
marcopeocchi <marco.piovanello@gmail.com>
2023-09-23 21:35:15 +0200

    migrated to ESM from CJS

--- src/core/http.ts
+++ src/core/http.ts
@@ -1,4 +1,4 @@
-import { get } from 'https';
+import { get } from 'https'
 
 module myHttp {
   export function json<T>(hostname: string, {
@@ -12,35 +12,35 @@ module myHttp {
         headers,
         method: 'GET',
         port: 443,
-      };
-      let chunks: any[] = [];
+      }
+      let chunks: any[] = []
       get(options, (res) => {
         // push the http packets chunks into the buffer
         res.on('data', (chunk) => {
-          chunks.push(chunk);
-        });
+          chunks.push(chunk)
+        })
         // the connection has ended so build the body from the buffer
         // parse it as a JSON and get the tag_name
         res.on('end', () => {
-          const buffer = Buffer.concat(chunks);
+          const buffer = Buffer.concat(chunks)
           try {
-            const data = JSON.parse(buffer.toString());
+            const data = JSON.parse(buffer.toString())
             if (!data.error) {
-              resolve(data.values);
+              resolve(data.values)
             }
-            reject(data.error);
+            reject(data.error)
           }
           catch (err) {
-            reject(err);
+            reject(err)
           }
         })
         res.on('error', (error) => {
-          reject(error);
-        });
+          reject(error)
+        })
       }).on('error', (error) => {
-        reject(error);
-      });
-    });
+        reject(error)
+      })
+    })
   }
   export function html(hostname: string, {
     path,
@@ -56,22 +56,22 @@ module myHttp {
         headers,
         method: 'GET',
         port: 443,
-      };
-      let chunks: any[] = [];
+      }
+      let chunks: any[] = []
       get(options, (res) => {
         res.on('data', (chunk) => {
-          chunks.push(chunk);
-        });
+          chunks.push(chunk)
+        })
         res.on('end', () => {
           resolve(Buffer.concat(chunks).toString())
         })
         res.on('error', (error) => {
-          reject(error);
-        });
+          reject(error)
+        })
       }).on('error', (error) => {
-        reject(error);
-      });
-    });
+        reject(error)
+      })
+    })
   }
 }
 
